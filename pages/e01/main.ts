0248f38cf33b2f2930a9e59da8d3fc5824331ef5
Vasil Genov <vasil.genov@psinteractive.io>
2023-04-23 10:46:59 +0300

    basic types fin

--- pages/e01/main.ts
+++ pages/e01/main.ts
@@ -4,3 +4,21 @@ let myName = 'Vasil'
 let myName2: string = 'Vasil'
 // @ts-expect-error
 myName2 = 42
+
+let meaningOfLife: number
+let isLoading: boolean
+let album: string | number
+
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+// return type is inferred
+const sum = (a: number, b: number) => {
+  return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean | string
+
+let re: RegExp = /\w+/g
